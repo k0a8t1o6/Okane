5ee48b919e0d6b01bb32b8ef897d08fce3b0b9b9
Takashi Kato <46238149+k0a8t1o6@users.noreply.github.com>
2023-01-22 21:38:06 +0900

    Initial Commit

---
+++ Sources/Okane/Okane.swift
@@ -0,0 +1,6 @@
+public struct Okane {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/OkaneTests/OkaneTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Okane
+
+final class OkaneTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Okane().text, "Hello, World!")
+    }
+}
